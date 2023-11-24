ca3c1d7106bad95395d560f85dce05d28890fedb
Rufus251 <vladimerrybalkin@gmail.com>
2023-11-24 19:37:10 +1000

    added manager-form component and update it in index

--- client/index/src/components/ContactForm.vue
+++ client/index/src/components/ContactForm.vue
@@ -1,74 +1,14 @@
 <template>
-  <div class="bgColor">
+  <section class="bgColor">
     <div class="contactForm">
       <h2>Заполните форму обратной связи, если у вас остались вопросы</h2>
 
-      <v-form v-model="valid" ref="form">
-        <v-autocomplete
-          label="Выберите менеджера"
-          :items="managers"
-          variant="underlined"
-        ></v-autocomplete>
-
-        <v-text-field
-          v-model="name"
-          :rules="nameRules"
-          label="Имя"
-          variant="underlined"
-        >
-        </v-text-field>
-
-        <v-text-field
-          v-model="number"
-          :rules="numberRules"
-          label="8 (800) 000-00-00"
-          variant="underlined"
-        >
-        </v-text-field>
-
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          label="E-mail"
-          variant="underlined"
-        >
-        </v-text-field>
-
-        <v-checkbox
-          v-model="checkbox"
-          :rules="[(v) => !!v || 'Обязательное поле']"
-          label="Я согласен с условиями обработки персональных данных."
-          required
-          hide-details
-          class="caption"
-        ></v-checkbox>
-
-        <blue-button-full :isDisabled="!valid">
-          Получить консультацию
-        </blue-button-full>
-
-      </v-form>
+      <ManagerForm></ManagerForm>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      valid: "",
-      managers: ["Менеджер 1", "Менеджер 2", "Менеджер 3"],
-
-      name: "",
-      nameRules: [],
-      number: "",
-      numberRules: [],
-      email: "",
-      emailRules: [],
-      checkbox: false,
-    };
-  },
-};
 </script>
 
 <style scoped lang="scss">
@@ -105,33 +45,6 @@ export default {
       margin-right: auto;
       margin-left: auto;
     }
-  }
-
-  .v-form {
-    max-width: 570px;
-    width: 100%;
-
-    padding-top: 40px;
-    padding-left: 60px;
-    padding-bottom: 40px;
-    padding-right: 60px;
-
-    background-color: #fff;
-    border-radius: 12px;
-    box-shadow: 0px 8px 30px 0px rgba(0, 0, 0, 0.05);
-
-    @media (max-width: 1300px) {
-      margin-top: 20px;
-      padding: 20px;
-
-      margin-right: auto;
-      margin-left: auto;
-    }
-
-    .v-btn{
-      margin-top: 30px;
-    }
-    // v-label settings for checkbox in App.vue
   }
 }
 </style>
