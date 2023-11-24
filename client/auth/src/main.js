93a99c30fc6e872e442ad8b22c12c7905d913d81
Rufus251 <vladimerrybalkin@gmail.com>
2023-11-24 20:26:52 +1000

    update ui

--- client/auth/src/main.js
+++ client/auth/src/main.js
@@ -6,9 +6,25 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+import { mdi } from "vuetify/lib/iconsets/mdi";
+import "@mdi/font/css/materialdesignicons.css";
+
+const app = createApp(App);
+
+import UIComponents from "@/components/UI/index.js";
+UIComponents.forEach((component) => {
+  app.component(component.name, component);
+});
+
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: "mdi",
+    sets: {
+      mdi,
+    },
+  },
 });
 
-createApp(App).use(vuetify).mount("#app");
+app.use(vuetify).mount("#app");
